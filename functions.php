f2dad3ba2b4f94bed737a99c61e6c259d892a2ad
Stephen Schrauger <stephen@schrauger.com>
2018-08-28 13:56:09 -0400

    styles

--- functions.php
+++ functions.php
@@ -9,5 +9,9 @@
 // Custom shortcodes
 get_template_part( 'includes/shortcodes/functions' );
 
+function include_child_styles() {
+    wp_enqueue_style( 'colleges-theme-child-ucfh-intranet', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'include_child_styles' );
 
 ?>
