e9f310e1543376409a8294c9dc5fc75abdc90237
Stephen Schrauger <stephen@schrauger.com>
2018-08-28 14:15:41 -0400

    fixes tax name

--- includes/taxonomies.php
+++ includes/taxonomies.php
@@ -7,16 +7,21 @@
  */
 
 add_action( 'init', 'create_taxonomy' );
-
 function create_taxonomy() {
+	$labels = array(
+		'name' => _x( 'Categories', 'taxonomy general name' ),
+		'singular_name' => _x( 'Category', 'taxonomy singular name' ),
+		'all_items' => __( 'All Categories' ),
+		'edit_item' => __( 'Edit Category' ),
+		'update_item' => __( 'Update Category' ),
+		'add_new_item' => __( 'Add New Category' ),
+		'new_item_name' => __( 'New Category Name' ),
+		'menu_name' => __( 'Categories' )
+	);
 
-	register_taxonomy( 'page_shortcode_taxonomy', 'page', array(
+	register_taxonomy( 'page_category', 'page', array(
 		'hierarchical' => true,
-		'labels' => array(
-			'name' => _x( 'Shortcode Taxonomy', 'taxonomy general name'),
-			'menu_name' => __('Shortcode Taxonomy')
-		)
+		'labels' => $labels
 	));
-
 
 }
